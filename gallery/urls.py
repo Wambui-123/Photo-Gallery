f53b1a91b297ac2e3de30ad441fa41ab0e640577
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-05-30 14:07:03 +0300

    deleted unused files

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from .views import gallery_upload, index, gallery_create, gallery_update, gallery_delete
+from .views import  gallery_update, gallery_delete
 from . import views
 
 app_name = 'gallery'
 
 urlpatterns = [
-        path('upload/', gallery_upload, name='gallery_upload'),
-        path('index/', index, name='index'),
+        # path('upload/', gallery_upload, name='gallery_upload'),
+        # path('index/', index, name='index'),
         # path('create/',LeadCreateView.as_view(), name='lead-create'),
-        path('create/', gallery_create, name='gallery_create'),
+        # path('create/', gallery_create, name='gallery_create'),
         # path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
         # path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
         path('<int:pk>/update/', gallery_update, name='gallery_update'),
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Image
-from .forms import ImageForm, ImageModelForm  
+from .forms import ImageForm
 # Create your views here.
 
 def landing_page(request):
@@ -16,28 +16,12 @@ def landing_page(request):
             return render(request,'landing.html',{"img":img,"form":form})
     return render(request, 'landing.html')
 
-
-def index(request):
-    if request.method == "POST":
-        form=ImageForm(data=request.POST,files=request.FILES)
-        if form.is_valid():
-            form.save()
-            obj=form.instance
-            return render(request,'landing.html',{"obj":obj})
-        else:
-            form=ImageForm()
-            img=Image.objects.all()
-            return render(request,'landing.html',{"img":img,"form":form})
-    return render(request,'landing.html')
-    
-
-
 # creating form
 def gallery_upload(request):
-    form = ImageModelForm()
+    form = ImageForm()
     if request.method == "POST":
         print('Recieving Post Request')
-        form = ImageModelForm(request.POST)
+        form = ImageForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/gallery_upload')
@@ -47,11 +31,11 @@ def gallery_upload(request):
     return render(request, 'gallery/gallery_upload.html', context)
 
 def gallery_create(request):
-    form = ImageModelForm()
+    form = ImageForm()
     img=Image.objects.all()
     if request.method == "POST":
         print('Recieving Post Request')
-        form = ImageModelForm(request.POST)
+        form = ImageForm(request.POST)
         if form.is_valid():
             form.save()
             return render(request,'landing.html',{"img":img,"form":form})
@@ -64,21 +48,22 @@ def gallery_create(request):
 
 def gallery_update(request, pk):
     gallery = Image.objects.get(id=pk)
-    form = ImageModelForm(instance = gallery)
+    form = ImageForm(instance = gallery)
     if request.method == "POST":
         print('Recieving Post Request')
         form=ImageForm(data=request.POST,files=request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('/gallery')
+            return redirect('/landing_')
     context = {
         "form": form,
          "gallery": gallery
     }
     return render(request, "gallery/gallery_update.html", context)
 
+
 def gallery_delete(request, pk):
     gallery = Image.objects.get(id=pk)
     gallery.delete()
-    return redirect('/gallery')
+    return redirect('landing.html')
 
